f0cd8d44cf49876d24524b7a065952c914aa7a5f
lucka_a <anna.lucka2002@gmail.com>
2025-04-03 20:02:02 +0200

    wczytanie danych hydro

--- notebooks/imgw_api.py
+++ notebooks/imgw_api.py
@@ -1,4 +1,9 @@
 import pandas as pd
+import requests
+import io
+import zipfile
+import chardet
+
 
 def get_hydro_metadata():
     hydro_url = "http://danepubliczne.imgw.pl/api/data/hydro2/"
@@ -9,3 +14,37 @@ def get_meteo_metadata():
     meteo_url = "http://danepubliczne.imgw.pl/api/data/meteo/"
     meteo_stat = pd.read_json(meteo_url)
     return meteo_stat.iloc[:, 0:4]
+
+def get_hydro_data(first, last):
+    
+    hydro_data = pd.DataFrame()
+    for i in range(first,last+1):
+        for j in range(1,13):
+            if i != 2023:
+                sep = ","
+                if j < 10:
+                    url = f"https://danepubliczne.imgw.pl/data/dane_pomiarowo_obserwacyjne/dane_hydrologiczne/dobowe/{i}/codz_{i}_0{j}.zip" 
+                else:
+                    url = f"https://danepubliczne.imgw.pl/data/dane_pomiarowo_obserwacyjne/dane_hydrologiczne/dobowe/{i}/codz_{i}_{j}.zip" 
+            else:
+                url = f"https://danepubliczne.imgw.pl/data/dane_pomiarowo_obserwacyjne/dane_hydrologiczne/dobowe/{i}/codz_{i}.zip" 
+                sep = ";"
+            
+            response = requests.get(url)
+            if response.status_code == 200:
+                zip_file = zipfile.ZipFile(io.BytesIO(response.content))  
+            
+                with zip_file.open(zip_file.namelist()[0]) as file:
+                    content = file.read() 
+                    encoding = chardet.detect(content)["encoding"]
+                    raw_data = content.decode(encoding, errors="replace")  
+                    data = io.StringIO(raw_data)
+                    data_month = pd.read_csv(data, header=None, encoding=encoding, sep=sep, lineterminator="\n")
+                    hydro_data = pd.concat([hydro_data, data_month])
+                if i == 2023:
+                    break
+            else:
+                print(f"Nie udało się pobrać pliku ZIP.") 
+    
+    hydro_data.columns = ['Station Code','Station Name','Name','Hydro Year','Hydro Month','Day','Water Level','Flow','Water Temp','Calendar Month']
+    return hydro_data
